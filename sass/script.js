57325aa0caac50c38fd3a1c040d8bc721ee0825e
Glodi Mboko <gmboko664@gmail.com>
2021-07-09 17:14:35 +0100

    modify portfolio

---
+++ sass/script.js
@@ -0,0 +1,40 @@
+const displays = document.querySelectorAll('.note-display');
+const transitionDuration = 1000;
+console.log(displays)
+displays.forEach(display => {
+  let note = parseInt(display.dataset.note);
+  let [int, dec] = display.dataset.note.split('.');
+  [int, dec] = [Number(int), Number(dec)];
+    console.log(note)
+  strokeTransition(display, note);
+
+  increaseNumber(display, int, 'int');
+  //increaseNumber(display, dec, 'dec');
+});
+
+
+function strokeTransition(display, note) {
+       let progress = display.querySelector('.circle__progress--fill');
+       let radius = progress.r.baseVal.value;
+       let circumference = 2 * Math.PI * radius;
+       let offset = circumference * (100 - note) / 100;
+    
+       progress.style.setProperty('--initialStroke', circumference);
+       progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
+    
+       setTimeout(() => progress.style.strokeDashoffset = offset, 100);
+ }
+
+ function increaseNumber(display, number, className) {
+    let element = display.querySelector(`.percent__${className}`),
+        decPoint = className === 'int' ? '%' : '',
+        interval = transitionDuration / number,
+        counter = 0;
+  
+    let increaseInterval = setInterval(() => {
+    if (counter === number) { window.clearInterval(increaseInterval); }
+    element.textContent = counter + decPoint;
+    counter++;
+    }, interval);
+  }
+  
